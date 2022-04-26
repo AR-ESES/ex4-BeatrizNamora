5fd1d765f460472aa38bf1550af1e5beac174235
BeatrizNamora <102222010+BeatrizNamora@users.noreply.github.com>
2022-04-26 11:39:55 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,23 +1,62 @@
-let espLarg = 20;
-let espAltura = 20;
-
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(0);
-  stroke(255);
-  strokeWeight(2);
-  noFill();
-  background(0);
+	createCanvas(windowWidth, windowHeight);
+	background(250,190,247);
+	noStroke();
+	fill(255);
+	background(237,185,224);
+	rectMode(CENTER);
 }
 
 function draw() {
-  
-  for (let i = 1; i < espLarg; i++) {
-    for (let j = 1; j < espAltura; j++) {
-      push();
-      translate(i * width / espLarg,j * height / espAltura)
-      circle(0, 0, height / espAltura);
-      pop();
-    }
-  }
+	//background(237,185,224);
+	rect(mouseX, height - 20, 150, 20);
 }
+
+function mouseClicked() {
+background (237,185,224);
+	
+	fill(253, 246, 227);
+	circle (100, 100, 80);
+
+	fill(118,81,180);
+	ellipse(100, 215, 55, 150);
+
+	arc(144, 185, 70, 70, 0, PI + QUARTER_PI, PIE);
+
+	fill(0,0,0);
+	rect(130, 285, 50, 20, 20, 15, 10, 5);
+
+	circle (120, 90, 10);
+
+	 ellipse(140, 105, 15, 10);
+
+	line(130, 115, 120, 130);
+	// put drawing code here
+	function setup() {
+
+	  }
+  fill(255,255,255);
+  circle(random(width), random(height), 10);
+
+  circle(random(width), random(height), 10);
+  
+  circle(random(width), random(height), 10);
+  
+  circle(random(width), random(height), 10);
+
+  circle(random(width), random(height), 10);
+  
+  circle(random(width), random(height), 10);
+  
+    circle(random(width), random(height), 10);
+
+  circle(random(width), random(height), 10);
+  
+  circle(random(width), random(height), 10);
+  
+  circle(random(width), random(height), 10);
+
+  circle(random(width), random(height), 10);
+  
+  circle(random(width), random(height), 10);
+	  }
